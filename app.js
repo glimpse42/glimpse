78ff794eec22ed70d60b1ab480d37358c8e86001
Jordan Dodson <jordandodson22@gmail.com>
2017-10-21 11:02:15 -0500

    adding the dashboard

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
 let index = require('./routes/index');
-let users = require('./routes/users');
+let user = require('./routes/user');
 
 let app = express();
 
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,8 +26,9 @@ app.use((req, res, next) => {
     next();
 });
 
+// Routes
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
